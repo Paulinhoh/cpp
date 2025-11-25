90f3826a7a1ab54c9f79b725ade99c052604cbc5
Paulo Henrique Reis <phdsreis@gmail.com>
2025-11-24 21:47:23 -0300

    arrays

--- aulas/aula_15/aula_15.cpp
+++ aulas/aula_15/aula_15.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
+    // vetor padrão c++
+    int vetor[4] = {7, 1, 22, -2};
+
+    // vetor usando std (vector)
+    vector<int> vetor2(3);
+
+    for (auto &&i : vetor)
+    {
+        cout << i << endl;
+    }
+    cout << endl;
+
+    for (auto &&i : vetor2)
+    {
+        cout << i << endl;
+    }
+    cout << endl;
+
+    // o sizeof retorna o tamanho do tipo no caso int cada bite é 4
+    cout << "tamanho do vetor: " << sizeof(vetor) / 4 << endl;
+    cout << "tamanho do vetor2: " << vetor2.size() << endl;
+
     return 0;
 }
---
+++ aulas/aula_16/aula_16.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int matriz[3][4];
+
+    matriz[0][0] = 0;
+    matriz[0][1] = 0;
+    matriz[0][2] = 0;
+    matriz[0][3] = 0;
+
+    matriz[1][0] = 1;
+    matriz[1][1] = 1;
+    matriz[1][2] = 1;
+    matriz[1][3] = 1;
+
+    matriz[2][0] = 2;
+    matriz[2][1] = 2;
+    matriz[2][2] = 2;
+    matriz[2][3] = 2;
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j < 4; j++)
+        {
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
